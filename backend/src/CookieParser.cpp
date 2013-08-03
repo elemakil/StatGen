2c9169b43c47db35267cc8cdade850c8085afc33
Fabian Wilk <mail@fabianwilk.de>
2013-08-04 01:47:06 +0200

    Moved DBG output to print

--- backend/src/CookieParser.cpp
+++ backend/src/CookieParser.cpp
@@ -7,7 +7,6 @@ CookieParser::CookieParser( char * cookies ){
 	    auto splitPos = vsCookies.at( iCookie ).find( "=" );
 	    std::string sKey = vsCookies.at( iCookie ).substr( 0, splitPos );
 	    std::string sData = vsCookies.at( iCookie ).substr( splitPos + 1, std::string::npos );
-	    std::cout << "<!-- Found Key \"" << sKey << "\" with data \"" << sData << "\" -->" << std::endl;
 	    // check for empty data / whitespace data
 	    if ( sData == "" || sData.find_first_not_of(' ') != std::string::npos ){
 		continue;
@@ -23,7 +22,7 @@ CookieParser::CookieParser( char * cookies ){
 
 /* virtual */ void CookieParser::Print( std::ostream & aStream /* = std::cout */ ){
     for ( auto it=m_mssData.begin(); it!=m_mssData.end(); ++it ){
-	aStream << "\"" << it->first << "\" -> \"" << it->second << "\"" << std::endl;
+	aStream << "<!-- \"" << it->first << "\" -> \"" << it->second << "\" -->" << std::endl;
     }
 }
 
