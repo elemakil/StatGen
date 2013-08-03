dfd1482db4f7c12e36e19f11c082cc2e961fe21a
Fabian Wilk <mail@fabianwilk.de>
2013-08-04 01:29:00 +0200

    FIXED CookieParser: Check for Null pointer in ctor

--- backend/src/CookieParser.cpp
+++ backend/src/CookieParser.cpp
@@ -1,16 +1,18 @@
 #include "CookieParser.hpp"
 
 CookieParser::CookieParser( char * cookies ){
-    std::vector<std::string> vsCookies = Split( std::string( cookies ), ';' );
-    for ( size_t iCookie=0; iCookie<vsCookies.size(); ++iCookie ){
-	auto splitPos = vsCookies.at( iCookie ).find( "=" );
-	std::string sKey = vsCookies.at( iCookie ).substr( 0, splitPos );
-	std::string sData = vsCookies.at( iCookie ).substr( splitPos + 1, std::string::npos );
-	// check for empty data / whitespace data
-	if ( sData == "" || sData.find_first_not_of(' ') != std::string::npos ){
-	    continue;
+    if ( cookies ){
+	std::vector<std::string> vsCookies = Split( std::string( cookies ), ';' );
+	for ( size_t iCookie=0; iCookie<vsCookies.size(); ++iCookie ){
+	    auto splitPos = vsCookies.at( iCookie ).find( "=" );
+	    std::string sKey = vsCookies.at( iCookie ).substr( 0, splitPos );
+	    std::string sData = vsCookies.at( iCookie ).substr( splitPos + 1, std::string::npos );
+	    // check for empty data / whitespace data
+	    if ( sData == "" || sData.find_first_not_of(' ') != std::string::npos ){
+		continue;
+	    }
+	    m_mssData[ sKey ] = sData;
 	}
-	m_mssData[ sKey ] = sData;
     }
 }
 
