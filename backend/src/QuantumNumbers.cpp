1f9902aab1e84898678e0a90ac2e0138db8950cd
Fabian Wilk <mail@fabianwilk.de>
2013-08-04 02:40:36 +0200

    WIP

--- backend/src/QuantumNumbers.cpp
+++ backend/src/QuantumNumbers.cpp
@@ -1,7 +1,7 @@
 #include "QuantumNumbers.hpp"
 
 const char * Flavour::Names [] = { "u", "d", "s", "c", "t", "b", "U", "D", "S", "C", "T", "B", 0 };
-const char * Flavour::UNames [] = { "u", "d", "s", "c", "t", "b", "\u02C9 u", "D", "S", "C", "T", "B", 0 };
+const char * Flavour::UNames [] = { "u", "d", "s", "c", "t", "b", "\u02C9u", "D", "S", "C", "T", "B", 0 };
 const char * Spin::Names [] = { "↑", "↓", "↕", 0 };
 const char * Colour::Names [] = { "r", "g", "b", "w", "R", "G", "B", "W", "NoColour", 0 };
 const char * Handedness::Names [] = { "⇜", "⇝", "↭", 0 };
